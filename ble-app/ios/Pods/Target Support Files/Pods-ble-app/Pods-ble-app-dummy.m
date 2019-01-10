3403df83a12b9a9ba2586438b10666c96b57074c
Olivia Barnett <olivia@EZ-C02S5A8RGG7L-5.local>
2019-01-10 11:39:51 -0500

    ios setup

---
+++ ble-app/ios/Pods/Target Support Files/Pods-ble-app/Pods-ble-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ble_app : NSObject
+@end
+@implementation PodsDummy_Pods_ble_app
+@end
---
+++ ble-app/ios/ble-app/page.swift
@@ -0,0 +1,9 @@
+//
+//  page.swift
+//  ble-app
+//
+//  Created by Olivia Barnett on 1/10/19.
+//  Copyright © 2019 650 Industries, Inc. All rights reserved.
+//
+
+import Foundation
